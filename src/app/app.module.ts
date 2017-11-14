4f6f10a4a85a48531ce59ab1563889f935b1f93c
volkov.vladislav <vladislav.volkov@devexpress.com>
2017-11-14 09:21:32 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DxButtonModule } from 'devextreme-angular';
+
+//import { DxButtonModule } from 'devextreme-angular/ui/button';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+  BrowserAnimationsModule,
+    BrowserModule,
+
+    DxButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
